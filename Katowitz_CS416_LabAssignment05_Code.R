b0bcc5cc07ef120fbb48922e5db6833bc121072c
amkatow <akatowitz@students.columbiabasin.edu>
2026-05-23 17:10:02 -0700

    Add files via upload

--- Katowitz_CS416_LabAssignment05_Code.R
+++ Katowitz_CS416_LabAssignment05_Code.R
@@ -1,10 +1,14 @@
 plot(mtcars$wt, mtcars$mpg,
-+      main = "Car Weight vs Miles Per Gallon",
-+      xlab = "Weight",
-+      ylab = "Miles Per Gallon")
+     main = "Car Weight vs Miles Per Gallon",
+     xlab = "Weight",
+     ylab = "Miles Per Gallon")
+
+install.packages("ggplot2")
+library(ggplot2)
 
 ggplot(mtcars, aes(x = factor(cyl))) +
-+   geom_bar() +
-+   labs(title = "Number of Cars by Cylinder Count",
-+        x = "Number of Cylinders",
-+        y = "Count")
+  geom_bar() +
+  labs(title = "Number of Cars by Cylinder Count",
+       x = "Number of Cylinders",
+       y = "Count")
+
